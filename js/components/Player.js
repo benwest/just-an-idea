03d083049c79c98cafabf7fd81529a1feb9c8fd5
benwest <ben.s.west@gmail.com>
2020-02-25 13:21:26 +0100

    audio state fix

--- js/components/Player.js
+++ js/components/Player.js
@@ -36,15 +36,32 @@ module.exports = {
 		state.audioElement = new Audio();
 		state.audioElement.src = interview.audio;
 		state.audioElement.currentTime = initialTime;
-		state.onTimeUpdate = () => onTimeUpdate( state.audioElement.currentTime );
-		state.onEnd = () => {
-			state.onTimeUpdate();
-			onend();
-			state.playing = false;
-			m.redraw();
+		// state.onTimeUpdate = () => onTimeUpdate( state.audioElement.currentTime );
+		// state.onEnd = () => {
+		// 	state.onTimeUpdate();
+		// 	onend();
+		// 	state.playing = false;
+		// 	m.redraw();
+		// }
+		// state.audioElement.addEventListener( 'timeupdate', state.onTimeUpdate );
+		// state.audioElement.addEventListener( 'ended', state.onEnd )
+		state.audioEvents = {
+			timeupdate: () => onTimeUpdate( state.audioElement.currentTime ),
+			ended: () => {
+				onTimeUpdate( state.audioElement.currentTime );
+				onend();
+				state.playing = false;
+				m.redraw();
+			},
+			play: () => {
+				state.playing = true;
+				m.redraw();
+			},
+			pause: () => {
+				state.playing = false;
+				m.redraw();
+			}
 		}
-		state.audioElement.addEventListener( 'timeupdate', state.onTimeUpdate );
-		state.audioElement.addEventListener( 'ended', state.onEnd )
 		state.toggle = () => {
 			if ( !state.loaded ) {
 				video.src = interview.video[ window.innerWidth < PHONE_BREAKPOINT ? 0 : 1 ];
@@ -54,13 +71,17 @@ module.exports = {
 			state.playing
 				? state.audioElement.pause()
 				: state.audioElement.play();
-			state.playing = !state.playing;
+			// state.playing = !state.playing;
+		}
+		for ( let event in state.audioEvents ) {
+			state.audioElement.addEventlistener( event, state.audioEvents[ event ] );
 		}
 		state.toggle();
 	},
 	onbeforeremove: ({ state }) => {
-		state.audioElement.removeEventListener( 'timeupdate', state.onTimeUpdate );
-		state.audioElement.removeEventListener( 'ended', state.onEnd )
+		for ( let event in state.audioEvents ) {
+			state.audioElement.rmeoveEventlistener( event, state.audioEvents[ event ] );
+		}
 		return wait( 500 ).then( () => {
 			state.audioElement.src = '';
 		})
